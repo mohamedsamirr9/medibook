3829882f2f859fa93d3a38c3147325b3cbf72721
mohamedsamirr9 <mohamedsamir979797@gmail.com>
2026-05-29 16:51:58 +0300

    feat: add Doctor and Appointment models with DbContext configuration

--- backend/MediBook.API/Program.cs
+++ backend/MediBook.API/Program.cs
@@ -1,4 +1,7 @@
 
+using MediBook.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace MediBook.API
 {
     public class Program
@@ -6,6 +9,10 @@ namespace MediBook.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // Database
+            builder.Services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             // Add services to the container.
 
